7ec8bfadb704895386b63a766dff8541015c1f90
sowmik-sec <sowmik.sec@gmail.com>
2023-02-24 20:51:33 +0600

    show error and reset form

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { app } from "../firebase/firebase.init";
@@ -7,17 +7,39 @@ import { app } from "../firebase/firebase.init";
 const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
+  const [passwordError, setPasswordError] = useState("");
+  const [success, setSuccess] = useState(false);
   const handleRegister = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    setSuccess(false);
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
     console.log(email, password);
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError("Please provide at least two uppercase letters");
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordError("Password should be at least 6 characters");
+      return;
+    }
+    if (!/(?=.*[!@#$&*])/.test(password)) {
+      setPasswordError("Please add at least one special character");
+      return;
+    }
+    setPasswordError("");
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setSuccess(true);
+        form.reset();
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setPasswordError(error.message);
+      });
   };
   return (
     <div className="w-50 mx-auto">
@@ -42,7 +64,8 @@ const RegisterReactBootstrap = () => {
             required
           />
         </Form.Group>
-
+        <p className="text-danger">{passwordError}</p>
+        {success && <p className="text-success">User Created Successfully</p>}
         <Button variant="primary" type="submit">
           Register
         </Button>
